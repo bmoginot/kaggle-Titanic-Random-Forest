0c207028fdd7c4b0650c55d5a7ccc57258e8294b
Ben Moginot <bmoginot5@gmail.com>
2025-01-24 11:43:01 -0600

    optimize parameters

--- rf-model.py
+++ rf-model.py
@@ -4,8 +4,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 
-pass_data = pd.read_csv("/kaggle/input/titanic/train.csv")
+pass_data = pd.read_csv("train.csv")
 pass_data.head()
+
+seed = 23
 
 init_features = ["Survived", "Pclass", "Sex", "Age", "SibSp", "Parch", "Embarked"] # choose relevant features
 pass_data = pass_data[init_features]
@@ -31,12 +33,29 @@ y = pass_data["Survived"] # values to predict
 
 X.shape, y.shape # remember to clean up data before defining variables
 
-train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=23)
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=seed)
 
-rf_model = RandomForestRegressor(random_state=23) # initialize random forest model
+# Hyperparameters
+n_estimators = 300
+max_depth = 18
+min_samples_split = 4
+min_samples_leaf = 2
+
+rf_model = RandomForestRegressor(n_estimators=n_estimators, 
+                                 max_depth=max_depth, 
+                                 min_samples_leaf=min_samples_leaf, 
+                                 min_samples_split=min_samples_split, 
+                                 random_state=seed) # initialize random forest model
 
 rf_model.fit(train_X, train_y) # fit model to training data
 rf_predictions = rf_model.predict(val_X) # make predictions based on test data
 
 rf_mae = mean_absolute_error(rf_predictions, val_y) # compare predicted values against ground truth
+
+from csv import writer
+
+with open("log.csv", "a", newline="") as log:
+    log_writer = writer(log)
+    log_writer.writerow([n_estimators, max_depth, min_samples_split, min_samples_leaf, rf_mae])
+
 print(rf_mae)
